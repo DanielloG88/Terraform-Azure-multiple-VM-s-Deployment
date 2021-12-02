f0f157868d0d15c1eaede723c187dc3e467ddf48
Mappy88 <92573103+Mappy88@users.noreply.github.com>
2021-12-02 10:42:46 +0100

    Delete healt probe.tf

--- healt probe.tf
+++
@@ -1,6 +0,0 @@
-resource "azurerm_lb_probe" "example" {
-  resource_group_name = azurerm_resource_group.Rg_name.name
-  loadbalancer_id     = azurerm_lb.LoadBalancer.id
-  name                = "ssh-running-probe"
-  port                = 22
-}
